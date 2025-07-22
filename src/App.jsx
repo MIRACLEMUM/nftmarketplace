608e1f1fe270854c2dd30b583042c03c8bb05c8b
Miracle sunady <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-07-22 01:49:10 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center text-purple-600 mt-10">
+      NFT Marketplace is coming!
+    </div>
+  );
+}
+
+export default App;
